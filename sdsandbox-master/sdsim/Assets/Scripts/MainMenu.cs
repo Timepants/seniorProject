d88b1646af820c9b5c5cd6d2d023d9fa4a7910a5
Steven Hayden <sphayden00@gmail.com>
2019-02-24 22:40:26 -0600

    Unity menu works with Train and Predict Server Scripts

--- sdsandbox-master/sdsim/Assets/Scripts/MainMenu.cs
+++ sdsandbox-master/sdsim/Assets/Scripts/MainMenu.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -30,7 +32,77 @@ public class MainMenu : MonoBehaviour
     {
         string itemPath = "C:\"";
         itemPath = itemPath.Replace(@"/", @"\");   // explorer doesn't like front slashes
-        System.Diagnostics.Process.Start("explorer.exe", "/select," + itemPath);
+        System.Diagnostics.Process.Start("explorer.exe", "/select," + itemPath); 
 
+    }
+
+    public void OnTrain()
+    {
+        string title = "Train";
+        string message = "type in model name";
+        string model = EditorUtility.SaveFilePanel("Save Your model", "../../carStuff/src/carModels/", "My_Model", "h5");
+        print(model);
+        if (!string.IsNullOrEmpty(model))
+        {
+            OnPrepareData();
+            TrainModel(model);
+            
+        }
+        else
+        {
+            EditorUtility.DisplayDialog("Error!", "Please enter something in the correct format", "Ok");
+        }
+
+    }
+
+    private void TrainModel(string modelToSave)
+    {
+        
+        string path = "../../carStuff/src";
+        Process p = new Process();
+        ProcessStartInfo startInfo = new ProcessStartInfo();
+
+        startInfo.FileName = "cmd.exe";
+        startInfo.Arguments = "/c \"python " + path + "\\train.py\" " + modelToSave;
+        p.StartInfo = startInfo;
+        p.Start();
+
+    }
+
+    public void OnPrepareData()
+    {
+
+        string path = "../../carStuff/src";
+        Process p = new Process();
+        ProcessStartInfo startInfo = new ProcessStartInfo();
+
+        startInfo.FileName = "cmd.exe";
+        startInfo.Arguments = "/c \"python " + path + "\\prepare_data.py\"";
+        p.StartInfo = startInfo;
+        p.Start();
+    }
+
+    public void OnStartPredictServer()
+    {
+        string modelFolder = "../../carStuff/src/carModels/";
+
+        string modelpath = EditorUtility.OpenFilePanel("Choose model?? to deploy", modelFolder, "h5");
+        print(modelpath);
+
+        if (modelpath != null)
+        {
+
+            string path = "../../carStuff/src";
+            Process p = new Process();
+            ProcessStartInfo startInfo = new ProcessStartInfo();
+
+            startInfo.FileName = "cmd.exe";
+            startInfo.Arguments = "/c \"python " + path + "\\predict_server.py\" " + modelpath;
+            p.StartInfo = startInfo;
+            p.Start();
+        } else
+        {
+            EditorUtility.DisplayDialog("Error", "Please choose a proper file type!","Ok", "No");
+        }
     }
 }
